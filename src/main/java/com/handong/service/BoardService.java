03ee7b8cd9d4650ae13c519edc7bca1b843f41be
Administrator <Administrator@DESKTOP-ANH58E7>
2020-01-21 14:57:47 +0900

    crud

---
+++ src/main/java/com/handong/service/BoardService.java
@@ -0,0 +1,15 @@
+package com.handong.service;
+
+import java.util.List;
+
+import com.handong.domain.BoardVO;
+
+public interface BoardService {
+
+	public void regist(BoardVO board) throws Exception;
+	public BoardVO read(Integer bno) throws Exception;
+	public void modify(BoardVO board) throws Exception;
+	public void remove(Integer bno) throws Exception;
+	public List<BoardVO> listAll() throws Exception;
+	
+}
